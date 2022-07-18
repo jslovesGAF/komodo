7cd36a97eaf6b45e65a90e4be55073818a24c226
Joshua <joshua.sloves@gaf.com>
2022-07-18 15:27:57 -0400

    installer file

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "------ Running all neccesssary installations for KOMODO ------"
+
+#Wappalzyer 
+python3 -m install python-Wappalyzer
+
+#Art - for tprint
+pip install art==5.7
+
+#testssl 
+git clone --depth 1 https://github.com/drwetter/testssl.sh.git
+
+#Dotdotpwn 
+sudo apt install dotdotpwn
+
+#CheckURL
+git clone https://github.com/UndeadSec/checkURL.git
+
+#Python Dirbuster
+git clone https://github.com/coderj001/python-dirbuster.git
+cd python-dirbuster/
+pip install -r requirements.txt
+cd ..
+echo "------ Errors regarding 'urllib' can be disregarded. ------"
