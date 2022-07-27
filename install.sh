9da449f4cc12232ccc338e27e4c5d213088acb21
Joshua <joshua.sloves@gaf.com>
2022-07-27 15:01:19 -0400

    removed dirbuster, added cmseek

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ python3 -m pip install python-Wappalyzer
 
 #Art - for tprint
 pip install art==5.7
+pip install termcolor
 
 #testssl 
 git clone --depth 1 https://github.com/drwetter/testssl.sh.git
@@ -17,9 +18,10 @@ sudo apt install dotdotpwn
 #CheckURL
 git clone https://github.com/UndeadSec/checkURL.git
 
-#Python Dirbuster
-git clone https://github.com/coderj001/python-dirbuster.git
-cd python-dirbuster/
+#CMSeeK
+git clone https://github.com/Tuhinshubhra/CMSeeK
+cd CMSeeK
 pip install -r requirements.txt
+
 cd ..
-echo "------ Errors regarding 'urllib' can be disregarded. ------"
+
